2717d8265645b6fe2b6ba96a54b9d08abc32beec
shwethaj1104 <shwethaj1104@gmail.com>
2022-07-27 15:43:56 +0530

    initial commit and changes for generating random text

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import data from './data';
+
+function App() {
+  const [count,setCount] = useState(0)
+  const [text,setText] = useState([])
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    let amount = parseInt(count)
+    if(count<=0){
+      amount=1
+    }
+    if(count > data.length+1){
+      amount=data.length+1
+    }
+    setText(data.slice(0,amount))
+  }
+  
+  return (
+  <section className='section-center'>
+    <h3>generate random text</h3>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+      <label htmlFor='amount'>Paragraphs : </label>
+      <input type='number' name='amount' id='amount' value={count} onChange={(e)=>setCount(e.target.value)}></input>
+      <button type='submit' className='btn'>Generate</button>
+    </form>
+    <article className='lorem-text'>
+      {text.map((item,index)=>{
+        return <p key={index}>{text}</p>
+      })}
+    </article>
+  </section>
+    )
+}
+
+export default App;
